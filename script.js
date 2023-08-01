77414d51a407f40933937c637554e4d0f123363e
Vakho-web <vakhogvishiani@gmail.com>
2023-08-01 14:06:43 +0400

    ADD basline JS, FINALIZE calculator style

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-function add(a, b) {
-	return a + b;
-};
+	let previousValue = '';
+	let currentValue = '';
+	let operator = '';
 
-function subtract(a, b) {
-	return a - b;
-};
+document.addEventListener("DOMContentLoaded", function(){
+	//store variables for all HTML elements
+	let numbers = document.querySelectorAll(".number");
+	let clear = document.querySelector(".clear");
+	let equal = document.querySelector(".equal");
+	let decimal = document.querySelector(".decimal");
+	let operators = document.querySelectorAll(".operator");
 
-function multiplyract(a, b) {
-	return a * b;
-};
+	let previousScreen = document.querySelector(".previous");
+	let currentScreen = document.querySelector(".current");
 
-function divide(a, b) {
-	return a / b;
-};
 
-let var1;
-let var2;
-let operator;
+
+
+	function add(a, b) {
+		return a + b;
+	};
+	
+	function subtract(a, b) {
+		return a - b;
+	};
+	
+	function multiplyract(a, b) {
+		return a * b;
+	};
+	
+	function divide(a, b) {
+		return a / b;
+	};
+	
+	let var1;
+	let var2;
+	let operator;
+
+})
+
+
+
