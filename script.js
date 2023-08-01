19687b8207d34b5e5bf2a4bd0ef792790c01322e
Vakho-web <vakhogvishiani@gmail.com>
2023-08-01 13:40:08 +0400

    ADD general button layout and styling to index.html and styles.css

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+	return a + b;
+};
+
+function subtract(a, b) {
+	return a - b;
+};
+
+function multiplyract(a, b) {
+	return a * b;
+};
+
+function divide(a, b) {
+	return a / b;
+};
+
+let var1;
+let var2;
+let operator;
