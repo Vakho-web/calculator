656d05283aae393ae0d0ada14134fc5795753d71
Vakho-web <vakhogvishiani@gmail.com>
2023-08-01 15:14:44 +0400

    ADD basic calculator functionality

--- script.js
+++ script.js
@@ -13,30 +13,72 @@ document.addEventListener("DOMContentLoaded", function(){
 	let previousScreen = document.querySelector(".previous");
 	let currentScreen = document.querySelector(".current");
 
+	numbers.forEach((number) => number.addEventListener("click", function(e) {
+		handleNumber(e.target.textContent);
+		currentScreen.textContent = currentValue;
+	}))
 
+	operators.forEach((op) => op.addEventListener("click", function(e){
+		handleOperator(e.target.textContent);
+		previousScreen.textContent = previousValue + " " + operator;
+		currentScreen.textContent = currentValue;
+	}))
 
+	clear.addEventListener("click", function(){
+		previousScreen.textContent = '';
+		previousValue = '';
+		currentScreen.textContent = '';
+		currentValue = '';
+		operator = '';
+	})
 
-	function add(a, b) {
-		return a + b;
-	};
-	
-	function subtract(a, b) {
-		return a - b;
-	};
-	
-	function multiplyract(a, b) {
-		return a * b;
-	};
-	
-	function divide(a, b) {
-		return a / b;
-	};
-	
-	let var1;
-	let var2;
-	let operator;
-
+	equal.addEventListener("click", function(){
+		if(currentValue != "" && previousValue != "") {
+			calculate()
+			previousScreen.textContent = '';
+			if(previousValue.length <= 5) {
+				currentScreen.textContent = previousValue;
+			} else {
+				currentScreen.textContent = previousValue.slice(0,5) + "...";
+			}
+		}
+	})
 })
+
+function handleNumber(num) {
+	if(currentValue.length <= 5) {
+		currentValue += num;
+	}
+}
+
+function handleOperator(op) {
+	operator = op;
+	previousValue = currentValue;
+	currentValue = '';
+}
+
+function calculate(){
+	previousValue = Number(previousValue);
+	currentValue = Number(currentValue);
+
+	if(operator === "+") {
+		previousValue += currentValue;
+	} else if(operator === "-") {
+		previousValue -= currentValue;
+	} else if(operator === "*") {
+		previousValue *= currentValue;
+	} else if(operator === "/") {
+		previousValue /= currentValue
+	}
+
+	previousValue = roundNumber(previousValue);
+	previousValue = previousValue.toString();
+	currentValue = previousValue.toString();
+}
+
+function roundNumber(num) {
+	return Math.round(num * 1000) / 1000;
+}
 
 
 
